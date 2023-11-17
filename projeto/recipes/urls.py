dd4e4aa4ebf9bd635f6e73ee9112b6f3412a1a33
Gustavo Pimenta <gustav.alves17@gmail.com>
2023-11-16 21:56:01 -0300

    settings, urls, apps

---
+++ projeto/recipes/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from django.http import HttpResponse
+from recipes.views import home, contato, sobre
+
+
+urlpatterns = [
+    path('',home), #home
+    path('sobre/',sobre), #sobre
+    path('contato',contato), #contato     
+]
